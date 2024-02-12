8600a2a1183abd8b1d82a0a630d58a6b1deeda91
JesusEsteban2 <Jesus.Esteban@outlook.com>
2024-02-12 21:07:42 +0100

    home job

--- app/src/main/java/com/example/practica_calcu/MainActivity.kt
+++ app/src/main/java/com/example/practica_calcu/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var butNu7:Button
     lateinit var butNu8:Button
     lateinit var butNu9:Button
+    lateinit var butBack:Button
     lateinit var textResult:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,12 +32,22 @@ class MainActivity : AppCompatActivity() {
         butNu4=findViewById(R.id.butNu4)
         butNu5=findViewById(R.id.butNu5)
         butNu6=findViewById(R.id.butNu6)
-        butNu6=findViewById(R.id.butNu6)
-        butNu6=findViewById(R.id.butNu6)
-        butNu6=findViewById(R.id.butNu6)
+        butNu7=findViewById(R.id.butNu7)
+        butNu8=findViewById(R.id.butNu8)
+        butNu9=findViewById(R.id.butNu9)
+        butBack=findViewById(R.id.butBack)
 
+        butNu0.setOnClickListener {addDigit('0')}
         butNu1.setOnClickListener {addDigit('1')}
-
+        butNu2.setOnClickListener {addDigit('2')}
+        butNu3.setOnClickListener {addDigit('3')}
+        butNu4.setOnClickListener {addDigit('4')}
+        butNu5.setOnClickListener {addDigit('5')}
+        butNu6.setOnClickListener {addDigit('6')}
+        butNu7.setOnClickListener {addDigit('7')}
+        butNu8.setOnClickListener {addDigit('8')}
+        butNu9.setOnClickListener {addDigit('9')}
+        butBack.setOnClickListener {delDigit()}
 
         }
     private fun addDigit(c: Char) {
@@ -45,5 +56,11 @@ class MainActivity : AppCompatActivity() {
         } else {
             textResult.text=textResult.text.toString()+c.toString()
         }
+    }
+
+    private fun delDigit() {
+        var long=textResult.length()
+
+        textResult.text=textResult.text.subSequence(0..(long-2))
     }
 }

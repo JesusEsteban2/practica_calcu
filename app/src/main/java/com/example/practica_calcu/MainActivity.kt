d35b6c0da2eb88e096769683542786f361abb48f
mananas <jesus.esteban@outlook.com>
2024-02-12 14:02:59 +0100

    First commit code

--- app/src/main/java/com/example/practica_calcu/MainActivity.kt
+++ app/src/main/java/com/example/practica_calcu/MainActivity.kt
@@ -4,8 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    lateinit var butNu0
+    lateinit var butNu1
+    lateinit var butNu2
+    lateinit var butNu3
+    lateinit var butNu4
+    lateinit var butNu5
+    lateinit var butNu6
+    lateinit var butNu7
+    lateinit var butNu8
+    lateinit var textResult
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        butNu0=findViewById(R.id.butNu0)
+        butNu1=findViewById(R.id.butNu1)
+        butNu2=findViewById(R.id.butNu2)
+
+
     }
 }
